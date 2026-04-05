c866ee699b5ce1380bf0cfc1740e68a3df7e9fcd
SusmithaMereddy <mereddysusmitha2002@gmail.com>
2026-04-05 12:57:03 +0530

    changed variable name

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,5 +1,5 @@
 def add(a, b):
     return a + b
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-print("Sum:", add(num1, num2))
+num-1 = int(input("Enter first number: "))
+num-2 = int(input("Enter second number: "))
+print("Sum:", add(num-1, num-2))
